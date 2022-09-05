30e73432ce9aa63ba1e01a929155cea322da02ea
userGithubshan <112789284+userGithubshan@users.noreply.github.com>
2022-09-05 10:32:27 +0800

    pull master

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test1");
         System.out.println("hot test");
+        System.out.println("pull test");
     }
 }
